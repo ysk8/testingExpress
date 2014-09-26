324f6bdd4b9bc319f680deb9d94714349c19e790
ysk8 <gyesidr2sk8@gmail.com>
2014-09-26 22:31:14 +0000

    Added interaction to navs
    First interaction for buttons Marker and Calendar

--- prueba/public/javascripts/main.js
+++ prueba/public/javascripts/main.js
@@ -16,15 +16,37 @@ function onDocumentReady()
         e.preventDefault();
         console.log("Falta paso 1");
     }); */
+    
+    
+    
+    $("#nav1").hide();
+    $("#calendarioApp").hide();       
+    
+    
+    $("#addMarker").on('click', function(e)
+    {
+        $("#nav1").show();
         
-    $("#nav1").offset({left:200, top:100});
+        $("#nav1").offset({left:200, top:100});
+        
+        console.log("AddMarker!")
+                         
+    });
     
-    x=$("#nav1").offset().left;
-    y=$("#nav1").offset().top;
+    $("#calendario").on('click', function(e)
+    {
+        $("#calendarioApp").show();
+        
+        x=$("#nav1").offset().left;
+        y=$("#nav1").offset().top;
     
-    console.log("x = "+x+" y = "+y);
+        console.log("x = "+x+" y = "+y);
     
-    $("#calendarioApp").offset({left:x, top:y+80});
+        $("#calendarioApp").offset({left:x, top:y+80});
+        
+        console.log("Muestro los Dias!")
+                         
+    });
     
 }
 
